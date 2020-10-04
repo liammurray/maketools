9e0408cef651a7ad9d8902c4118d88a848fa66d1
Liam Murray <liammurray@users.noreply.github.com>
2020-10-04 12:56:27 -0700

    Update npm makefile

--- npm.mk
+++ npm.mk
@@ -12,41 +12,54 @@ build_files := $(source_files:src/%.ts=dist/%.js)
 # .package/<name>.tgz
 PACKAGE := .package/$(shell node -p 'const p=require("./package.json"); `$${p.name}-$${p.version}.tgz`')
 
+# For lerna: NPM_INSTALL=lerna bootstrap --scope <scope>
+NPM_INSTALL?=npm install
+
 .PHONY: \
 	build \
 	package \
   clean \
 	lint \
   utest \
-  develop \
+  server-develop \
+  server-debug \
+  watch
 
 build: dist
 
 package: $(PACKAGE)
 
 clean:
-	npm run build:clean
+	npm run compile:clean
 	rm -f $(PACKAGE)
 
-utest: node_modules
+utest: node_modules dist
 	npm run test
 
 lint: node_modules
 	npm run lint
 
-# For servers (express server, etc.)
-develop: node_modules
+# Make sure code is compiled, start server, watch for code changes
+server-develop: dist
 	npm run start:watch | pino-pretty
 
+# Make sure code is compiled, start server (no watch) with inspector port
+server-debug: dist
+	npm run start:debug | pino-pretty
+
+# Make sure code is compiled and watch for code changes
+watch: dist
+	npm run compile:watch
+
 node_modules: package.json
-	npm i && touch node_modules
+	$(NPM_INSTALL) && touch node_modules
 
 # For lambda functions
 lambda: build
 	npm run lambda
 
 dist: $(source_files) node_modules tsconfig.json
-	npm run build
+	npm run compile
 
 $(PACKAGE): $(build_files)
 	@mkdir -p "$(dir $@)"
